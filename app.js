681b8ecb4785457763aca69e71d4d0478f7e1a72
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-29 11:13:41 -0800

    made operate function

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ function divide(a,b) {
     return a/b;
 }
 
+function operate(a,b,sign) {
+    if (sign === "+") {
+        return add(a,b);
+    } else if (sign === "-") {
+        return subtract(a,b);
+    } else if (sign === "/") {
+        return divide(a,b);
+    } else if (sign === "*") {
+        return multiply(a,b);
+    }
+}
