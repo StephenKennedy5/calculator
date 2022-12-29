48ec05c7aa53a2a6679ef0aef09c25bdcc559269
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-29 12:55:17 -0800

    added function to numbers and clear

--- app.js
+++ app.js
@@ -25,3 +25,22 @@ function operate(a,b,sign) {
         return multiply(a,b);
     }
 }
+
+let display_value = '';
+
+function display() {
+    const numbers = document.querySelectorAll('.number')
+    numbers.forEach(value => value.addEventListener('click', function() {
+        display_value += String(value.innerHTML)
+        console.log(display_value)
+    }))
+    
+}
+
+function clear() {
+    const clear_val = document.querySelector('.clear')
+    clear_val.addEventListener('click', () => display_value = '');
+}
+
+display();
+clear();
